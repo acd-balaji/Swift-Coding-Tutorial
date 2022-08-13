3422a47bb1d8902dc51f233961eec0ca0811fa77
Balaji G <gbalajisuruthika@gmail.com>
2022-08-13 17:29:01 +0530

    LessonOneSwiftBasics
    Lesson One Swift Basics

---
+++ LessonOneSwiftBasics.playground/Contents.swift
@@ -0,0 +1,82 @@
+import UIKit
+
+let a = 10
+let b = 5
+let c = a + b
+print(c)
+
+// String Value
+var name: String = "Polo T-Shirt"
+
+// Int Value
+var quantity: Int = 10
+
+// Float Value
+var price: Float = 100.50
+
+// Bool Value
+var isSold: Bool = false
+
+quantity = 5
+price = 120.75
+name = "Polo T-Shirt Red"
+
+print(name)
+print("Name : \(name)")
+print(isSold)
+print("Sale Status : \(isSold)")
+print(quantity)
+print("Quantity : \(quantity)")
+print(price)
+print("Price : \(price)")
+
+
+// Product Name - String Value
+var productName: String
+productName = "Polo T-Shirt"
+productName = "Polo T-Shirt Red"
+//productName = 10
+
+// Product Quantity - Int Value
+var productQuantity: Int
+
+// Product Price - Float Value
+var productPrice: Float
+
+// Product Sale Status - Bool Value
+var productIsSold: Bool
+
+// Test Property
+let test: String
+
+
+// Product Name - String Value
+var proName = ""
+//proName = 10
+
+// Product Price - Float Value
+var proPrice = 100.50
+
+// Product Quantity - Int Value
+var proQuantity = 10
+
+
+// Single line comment
+
+/* Multi
+ line
+ comment
+ */
+
+/// Single line viewable comment
+var studentName = "Ajju"
+studentName = "AJJU"
+
+/**
+ Multi
+ line
+ viewable
+ comment
+ */
+var studentClass = "1st std"
+studentClass = "1st STD"
